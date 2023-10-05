b4124755a9c664b9aa0165c3e4aff9dc6cce6b91
viniduminsara <viniduminsara@gmail.com>
2023-10-05 20:24:16 +0530

    complete CRUD in customer

--- controller/customerController.js
+++ controller/customerController.js
@@ -4,53 +4,64 @@ const customer_Id = $('#customerId');
 const full_name = $('#fullname');
 const address = $('#address');
 
-const customer_submit = $('#customer_submit');
-const customer_update = $('#customer_update');
-const customer_delete = $('#customer_delete');
-const customerReset = $("button[type = 'reset']").eq(0);
+const customer_btn = $('#customer_btn button');
 
-customer_submit.on('click', () => {
+let customer_db = [];
+
+customer_btn.eq(0).on('click', () => {
     let customerId = customer_Id.val().trim();
     let fullName = full_name.val().trim();
     let addressVal = address.val().trim();
 
-    $('tbody').eq(0).append(
-        `<tr>
-            <th scope="row">${customerId}</th>
-            <td>${fullName}</td>
-            <td>${addressVal}</td>
+    let customer = {
+        id: customerId,
+        name: fullName,
+        address: addressVal
+    }
+    customer_db.push(customer);
+    loadCustomerTable();
+    customer_btn.eq(3).click();
+});
+
+customer_btn.eq(1).on('click', () => {
+    let customerId = customer_Id.val().trim();
+    let fullName = full_name.val().trim();
+    let addressVal = address.val().trim();
+
+    let customer = {
+        id: customerId,
+        name: fullName,
+        address: addressVal
+    }
+
+    let index = customer_db.findIndex(customer => customer.id === customerId);
+    customer_db[index] = customer;
+
+    loadCustomerTable();
+    customer_btn.eq(3).click();
+});
+
+customer_btn.eq(2).on('click', () => {
+    let customerId = customer_Id.val().trim();
+
+    let index = customer_db.findIndex(customer => customer.id === customerId);
+    customer_db.splice(index, 1);
+
+    loadCustomerTable();
+    customer_btn.eq(3).click();
+});
+
+const loadCustomerTable = function () {
+
+    $('tbody').eq(0).empty();
+    customer_db.map((index) => {
+        $('tbody').eq(0).append(
+            `<tr>
+            <th scope="row">${index.id}</th>
+            <td>${index.name}</td>
+            <td>${index.address}</td>
          </tr>`
-    );
-    customerReset.click();
-});
-
-customer_update.on('click', () => {
-    let customerId = customer_Id.val().trim();
-    let fullName = full_name.val().trim();
-    let addressVal = address.val().trim();
-
-    $('tbody').eq(0).find('tr').each(function() {
-
-        let cust_Id = $(this).find('th').text();
-
-        if (cust_Id === customerId) {
-            $(this).find('td:nth-child(2)').text(fullName);
-            $(this).find('td:nth-child(3)').text(addressVal);
-            customerReset.click();
-        }
+        );
     });
-});
 
-customer_delete.on('click', () => {
-    let customerId = customer_Id.val().trim();
-
-    $('tbody').eq(0).find('tr').each(function() {
-
-        let cust_Id = $(this).find('th').text();
-
-        if (cust_Id === customerId) {
-            $(this).remove();
-            customerReset.click();
-        }
-    });
-});
+}
