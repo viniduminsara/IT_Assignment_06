6a000eddb878794f7cb13e263a969ea22bbf8c83
viniduminsara <viniduminsara@gmail.com>
2023-10-07 06:47:45 +0530

    add dynamic search for customer

--- controller/customerController.js
+++ controller/customerController.js
@@ -5,26 +5,33 @@ import {customer_db} from '/db/db.js';
 const customer_Id = $('#customerId');
 const full_name = $('#fullname');
 const address = $('#address');
+const salary = $('#salary');
 const customer_btn = $('#customer_btn button');
+const  customer_search = $('#customer_search input');
+const  search_select = $('#customer_search select');
 
+//add customer
 customer_btn.eq(0).on('click', () => {
     let customerId = customer_Id.val().trim();
     let fullName = full_name.val().trim();
     let addressVal = address.val().trim();
+    let salaryVal = salary.val().trim();
 
-    let customer = new CustomerModel(customerId, fullName, addressVal);
+    let customer = new CustomerModel(customerId, fullName, addressVal, salaryVal);
 
     customer_db.push(customer);
     loadCustomerTable();
     customer_btn.eq(3).click();
 });
 
+//update customer
 customer_btn.eq(1).on('click', () => {
     let customerId = customer_Id.val().trim();
     let fullName = full_name.val().trim();
     let addressVal = address.val().trim();
+    let salaryVal = salary.val().trim();
 
-    let customer = new CustomerModel(customerId, fullName, addressVal);
+    let customer = new CustomerModel(customerId, fullName, addressVal,salaryVal);
 
     let index = customer_db.findIndex(customer => customer.customer_id === customerId);
     customer_db[index] = customer;
@@ -33,6 +40,7 @@ customer_btn.eq(1).on('click', () => {
     customer_btn.eq(3).click();
 });
 
+//delete customer
 customer_btn.eq(2).on('click', () => {
     let customerId = customer_Id.val().trim();
 
@@ -41,6 +49,37 @@ customer_btn.eq(2).on('click', () => {
 
     loadCustomerTable();
     customer_btn.eq(3).click();
+});
+
+//customer search
+customer_search.on('input', function() {
+    let option = search_select.find(":selected").text();
+    let index = customer_db.findIndex(customer => customer[option] === customer_search.val().trim());
+
+    if (index >= 0){
+        $('tbody').eq(0).empty();
+        $('tbody').eq(0).append(
+            `<tr>
+           <th scope="row">${customer_db[index].customer_id}</th>
+           <td>${customer_db[index].name}</td>
+           <td>${customer_db[index].address}</td>
+           <td>${customer_db[index].salary}</td>
+        </tr>`
+        );
+    }else{
+        loadCustomerTable();
+    }
+
+})
+
+$('tbody').eq(0).on('click', 'tr', function() {
+    let customerId = $(this).find('th').eq(0).text();
+    let index = customer_db.findIndex(customer => customer.customer_id === customerId);
+
+    customer_Id.val(customer_db[index].customer_id);
+    full_name.val(customer_db[index].name);
+    address.val(customer_db[index].address);
+    salary.val(customer_db[index].salary);
 });
 
 const loadCustomerTable = function () {
@@ -52,6 +91,7 @@ const loadCustomerTable = function () {
             <th scope="row">${index.customer_id}</th>
             <td>${index.name}</td>
             <td>${index.address}</td>
+            <td>${index.salary}</td>
          </tr>`
         );
     });
--- model/customerModel.js
+++ model/customerModel.js
@@ -1,7 +1,8 @@
 export class CustomerModel {
-    constructor(customer_id, name, address) {
+    constructor(customer_id, name, address, salary) {
         this.customer_id = customer_id;
         this.name = name;
         this.address = address;
+        this.salary = salary;
     }
 }
